80e68fa3894c928971358802dda7d2e58c10606e
Opeyemi Ajibade <eco.ajibade@gmail.com>
2023-11-16 01:32:15 -0800

    Write a function that prints a string, followed by a new line

---
+++ alx-low_level_programming/0x08-recursion/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+void_puts_recussion(char*s);
+int _putchar(char c);
+void_print_rev_rrecursion(char*s);
+int factorial(int n);
+int_pow_recursion(int x, int y);
+int_sqrt_recursion(int n);
+int is_prime_number(int n);
+int is_palindrome(char*s);
+#endif
