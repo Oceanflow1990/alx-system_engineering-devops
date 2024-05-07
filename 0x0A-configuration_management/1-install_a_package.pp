43c60bcebdeeadd26eae944b835ee0b109490190
oceanflow1990 <eco.ajibade@gmail.com>
2024-05-07 03:03:00 -0700

    0x0A-config

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,5 @@
-#!/usr/bin/pup
-#@ install a specific ersion of flask {2.1.0)
-pacckage {'flask':
-	ensure => '2.1.0,
-	povider => 'pip'
-}
+# install puppet-lint -v 2.5.0
 
+exec { 'puppet-lint':
+  command => '/usr/bin/apt-get -y install puppet-lint -v 2.5.0',
+}
